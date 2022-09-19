6453548b830be44a605e1b70d3c4e5835eea6650
Josh <joshuansanders@gmail.com>
2022-09-19 15:03:00 -0400

    moved services to page base

---
+++ SentientTwitter/Pages/PageBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using SentientTwitter.Services;
+using System;
+
+namespace SentientTwitter.Pages
+{
+    public class PageBase<T> : ComponentBase
+    {
+        [Inject]
+        protected TwitterService _twitterService { get; set; }
+        [Inject]
+        protected TextAnalyzerService _textAnalyzerService { get; set; }
+        [Inject]
+        protected IJSRuntime JsRuntime { get; set; }
+
+    }
+}
