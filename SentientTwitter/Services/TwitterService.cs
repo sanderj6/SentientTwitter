da0205ef5709ff2ff5aef1ebc00936a8da1f7c13
sanderj6 <joshuansanders@gmail.com>
2022-09-23 16:52:19 -0400

    refactored to align with SOLID techniques. Virtualize uses value task for perfomance

--- SentientTwitter/Services/TwitterService.cs
+++ SentientTwitter/Services/TwitterService.cs
@@ -13,6 +13,10 @@ using JsonSerializer = Newtonsoft.Json.JsonSerializer;
 using Microsoft.AspNetCore.Identity;
 using System.Threading;
 using System.Configuration;
+using System;
+using System.Diagnostics.Tracing;
+using System.Collections.Concurrent;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 namespace SentientTwitter.Services;
 
@@ -20,10 +24,17 @@ public interface ITwitterService
 {
     Task StartStream();
     Task StopStream();
+    Task ToggleStream(bool toggle);
+    Task CalculateTrending(TweetStreamData tweetStream);
+    TweetModel CreateTweetFromStream(TweetStreamData stream);
 }
 
 public class TwitterService
 {
+    public List<TweetModel> AllTweets { get; set; } = new();
+    public ConcurrentQueue<List<TweetModel>> coll = new ConcurrentQueue<List<TweetModel>>();
+    public ConcurrentQueue<TweetModel> coll2 = new ConcurrentQueue<TweetModel>();
+
     public delegate void OnTweetReceived(object sender, TweetEventReceived e);
     public event OnTweetReceived? TweetReceived;
 
@@ -32,7 +43,28 @@ public class TwitterService
     private HttpClient? httpClient;
     private string bearerToken;
 
-    public bool IsStreamOpen { get; set; } = true;
+    public Dictionary<Hashtag, int> HashTags { get; set; } = new();
+    public Dictionary<Domain, int> Topics { get; set; } = new();
+    public Dictionary<Entity, int> Subjects { get; set; } = new();
+
+    public List<string> TopHashtags { get; set; } = new();
+    public List<string> FilteredHashtags { get; set; } = new();
+    public string TrendingTopic { get; set; } = string.Empty;
+    public string TrendingSubject { get; set; } = string.Empty;
+
+    public bool IsStreamOpen
+    {
+        get
+        {
+            return _isStreamOpen;
+        }
+        set
+        {
+            _isStreamOpen = value;
+            ToggleStream(value);
+        }
+    }
+    private bool _isStreamOpen { get; set; } = true;
 
     public TwitterService(IConfiguration configuration)
     {
@@ -61,7 +93,6 @@ public class TwitterService
             httpClient.BaseAddress = new Uri("https://api.twitter.com/2/tweets/sample/stream?tweet.fields=context_annotations,lang,entities");
             httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
-            var _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
 
             // Stream
             using (httpClient)
@@ -77,10 +108,15 @@ public class TwitterService
                     {
                         try
                         {
-                            var tweet = JsonConvert.DeserializeObject<TweetStreamData>(streamReader.ReadLine());
-                            if (tweet is null || tweet.data is null) continue;
+                            var tweetStream = JsonConvert.DeserializeObject<TweetStreamData>(streamReader.ReadLine());
+                            if (tweetStream is null || tweetStream.data is null) continue;
 
-                            TweetReceived?.Invoke(this, new TweetEventReceived(tweet, "received"));
+                            var newTweet = CreateTweetFromStream(tweetStream);
+                            AllTweets.Add(newTweet);
+
+                            CalculateTrending(tweetStream).ConfigureAwait(false);
+
+                            TweetReceived?.Invoke(this, new TweetEventReceived(tweetStream, "received"));
 
                             await Task.Delay(1);
                         }
@@ -110,5 +146,110 @@ public class TwitterService
     {
         IsStreamOpen = false;
         await this.StartStream();
-    }    
+    }
+
+    public async Task ToggleStream(bool isOpen)
+    {
+        IsStreamOpen = isOpen;
+
+        //if (IsStreamOpen)
+        //{
+        //    await this.StartStream();
+        //}
+        //else
+        //{
+        //    await this.StopStream();
+        //}
+    }
+
+    // Calculate Hashtags, Domains, and Subjects
+    public async Task CalculateTrending(TweetStreamData tweetStream)
+    {
+        try
+        {
+            // Hashtags
+            if (tweetStream.data.entities is not null)
+            {
+                var hashtags = tweetStream.data.entities.hashtags;
+                if (hashtags is not null)
+                {
+                    foreach (var tag in hashtags)
+                    {
+                        var matchTag = HashTags.Where(x => x.Key.tag.ToUpper().Trim() == tag?.tag.ToUpper().Trim()).FirstOrDefault();
+                        if (matchTag.Key is null)
+                        {
+                            HashTags.Add(tag, 1);
+                        }
+                        else
+                        {
+                            HashTags[matchTag.Key] = matchTag.Value + 1;
+                        }
+                    }
+                }
+
+                TopHashtags = HashTags.OrderByDescending(x => x.Value).Select(y => y.Key.tag).Take(10).ToList();
+            }
+
+            // DOMAIN and ENTITY
+            if (tweetStream.data.context_annotations is not null)
+            {
+                foreach (var context in tweetStream.data.context_annotations)
+                {
+                    // TOPIC
+                    var topic = context.domain;
+                    if (topic is not null)
+                    {
+                        var match = Topics.Where(x => x.Key.name.ToUpper().Trim() == topic?.name.ToUpper().Trim()).FirstOrDefault();
+                        if (match.Key is null)
+                        {
+                            Topics.Add(topic, 1);
+                        }
+                        else
+                        {
+                            Topics[match.Key] = match.Value + 1;
+                        }
+
+                        TrendingTopic = Topics.OrderByDescending(x => x.Value).FirstOrDefault().Key.name.ToString();
+                    }
+
+                    // SUBJECT
+                    var subject = context.entity;
+                    if (subject is not null)
+                    {
+                        var match = Subjects.Where(x => x.Key.name.ToUpper().Trim() == subject?.name.ToUpper().Trim()).FirstOrDefault();
+                        if (match.Key is null)
+                        {
+                            Subjects.Add(subject, 1);
+                        }
+                        else
+                        {
+                            Subjects[match.Key] = match.Value + 1;
+                        }
+
+                        TrendingSubject = Subjects.OrderByDescending(x => x.Value).FirstOrDefault().Key.name.ToString();
+                    }
+                }
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Could not finish trending tasks: {ex}");
+        }
+    }
+
+    // Convert from Stream to Tweet Model
+    public TweetModel CreateTweetFromStream(TweetStreamData stream)
+    {
+        return new TweetModel()
+        {
+            Id = stream.data.id,
+            Text = stream.data.text,
+            Language = stream.data.lang,
+            Name = $"Tweet-{stream.data.id}",
+            Domain = stream.data.context_annotations is not null ? stream.data.context_annotations.FirstOrDefault()?.domain : null,
+            Entity = stream.data.context_annotations is not null ? stream.data.context_annotations.FirstOrDefault()?.entity : null,
+            Entities = stream.data.entities
+        };
+    }
 }
